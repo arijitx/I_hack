9c7f1feee1d69cd2f2ab6808028d4ac333af8838
zed41 <amkewlchamp@gmail.com>
2018-01-27 16:57:57 +0530

    web_appx

--- web_app/app.py
+++ web_app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from flask import send_file,session,jsonify
+import requests
 
 # set the project root directory as the static folder, you can set others.
 app = Flask(__name__, static_url_path='')
@@ -18,6 +19,12 @@ def save_user():
     session['u_name']=u_name
     return jsonify("{'status':'success'}")
 
+@app.route('/chat',methods=['GET', 'POST'])
+def chat():
+    msg=request.args.get('msg');
+    url='http://localhost:5005/conversations/default/parse?q=';
+    r = requests.get('http://localhost:5005/conversations/default/parse?q='+msg)
+    return jsonify(r.json())
 
 
 if __name__ == "__main__":
