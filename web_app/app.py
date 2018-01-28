7844c4b5fe8923d0d49b829c13b6b9fee08a3980
zed41 <amkewlchamp@gmail.com>
2018-01-28 12:09:46 +0530

    final_push

--- web_app/app.py
+++ web_app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from flask import send_file,session,jsonify
 import requests
+from response_gen import ResponseBuilder
 
 # set the project root directory as the static folder, you can set others.
 app = Flask(__name__, static_url_path='')
@@ -19,12 +20,27 @@ def save_user():
     session['u_name']=u_name
     return jsonify("{'status':'success'}")
 
+@app.route('/logout',methods=['GET', 'POST'])
+def logout():
+    del session['u_name']
+    return "logout"
+
 @app.route('/chat',methods=['GET', 'POST'])
 def chat():
     msg=request.args.get('msg')
     m_type=request.args.get('type')
     print('chat->',m_type)
     response={}
+    # if 'place' in msg or 'order' in msg or 'addtoorder' in msg or 'placeorder' in msg or 'repeatorder' in msg :
+    #     if(m_type == 'normal'):
+    #         url='http://localhost:5006/conversations/'+session['u_name']+'/parse?q='
+    #     else:
+    #         url='http://localhost:5006/conversations/'+session['u_name']+'/continue'
+    # else:
+    #     if(m_type == 'normal'):
+    #         url='http://localhost:5005/conversations/'+session['u_name']+'/parse?q='
+    #     else:
+    #         url='http://localhost:5005/conversations/'+session['u_name']+'/continue'
     if(m_type == 'normal'):
         url='http://localhost:5005/conversations/'+session['u_name']+'/parse?q='
     else:
@@ -34,14 +50,35 @@ def chat():
     else:
         post_data={"executed_action": msg, "events": []}
         r = requests.post(url,json=post_data)
-
-    response['action']=r.json()['next_action']
-    response['response']=action_to_string(response['action'])
+    if "action_suggest"==r.json()['next_action']:
+        response['action']="action_listen"
+    else:
+        response['action']=r.json()['next_action']
+    response['response']=action_to_string(response['action'],r.json()['tracker']['slots'],r.json()['tracker']['latest_message'])
     return jsonify(response)
 
-def action_to_string(action):
+def action_to_string(action,slots,msg):
+    print(action)
+    if action=='utter_placeorder':
+        return rb.action_placeorder(slots,msg,session['u_name'])
+    if action=='utter_repeatorder':
+        return rb.action_repeatorder(slots,msg,session['u_name'])
+    if action=='utter_addtoorder':
+        return rb.action_addtoorder(slots,msg,session['u_name'])
+
+    if 'action_listen'==action:
+        return "action_listen"
     if 'utter' in action:
+        return rb.utter(action)
+    if 'action' in action:
+        if(action=='action_search_restaurants'):
+            return rb.action_search_restaurants(slots)
+        if(action=='action_suggest'):
+            return "action_listen"
+
+    return ""
 
 
 if __name__ == "__main__":
-    app.run()
+    rb=ResponseBuilder()
+    app.run(host= '0.0.0.0')
--- web_app/response_gen.py
+++ web_app/response_gen.py
@@ -1,26 +1,68 @@
 import json
 import random
 import requests
+import pymongo
 
 class ResponseBuilder():
     def __init__(self):
         self.utter_map=json.load(open('utter.json'))
+        self.orders={}
 
     def utter(self,action):
-        responses=lf.utter_map[utter]
+        responses=self.utter_map[action]
         idx=random.randint(0,len(responses)-1)
         return responses[idx]
 
-    def action_factory(self,slots):
-        location=slots['city']
+    def action_search_restaurants(self,slots):
+        print(slots)
+        location=slots['location']
         cuisine=slots['cuisine']
         r=requests.get('https://developers.zomato.com/api/v2.1/locations?query='+location,headers={'user-key': 'fb627d5a1d8b3431f8b260310bbad9a6'})
         print(r.json())
         entity_id=r.json()['location_suggestions'][0]['entity_id']
-        url='https://developers.zomato.com/api/v2.1/search?entity_id='+str(entity_id)+'&cuisines='+cuisines
-        r=requests.get(url,headers={'user-key': 'fb627d5a1d8b3431f8b260310bbad9a6'})
-        response="<ul>"
-        for rx in r['restaurants']:
-            response+="<li>"+rx['name']+'</li>'
-        response+="</ul>"
-        return response
+        entity_type=r.json()['location_suggestions'][0]['entity_type']
+        url='https://developers.zomato.com/api/v2.1/search?entity_id='+str(entity_id)+'&cuisines='+cuisine+'&entity_type='+entity_type
+        r=requests.get(url,headers={'user-key': 'fb627d5a1d8b3431f8b260310bbad9a6'}).json()
+        info=[]
+        for rx in r['restaurants'][:3]:
+            info.append(rx['restaurant']['name'])
+            info.append(rx['restaurant']['user_rating']['aggregate_rating'])
+            info.append(rx['restaurant']['url'])
+        slider='<div class="well"><div id="myCarousel" class="carousel slide"><div class="carousel-inner"><div class="item active"><div class="row-fluid"><h3>{}</h3><h1>{}</h1><div class="span3"><a target="_blank" href={}><button  type="button" class="btn btn-success">Go</button></a></div></div></div><div class="item"><div class="row-fluid"><h3>{}</h3><h1>{}</h1><div class="span3"><a target="_blank" href={}><button  type="button" class="btn btn-success">Go</button></a></div></div></div><div class="item"><div class="row-fluid"><h3>{}</h3><h1>{}</h1><div class="span3"><a target="_blank" href={}><button  type="button" class="btn btn-success">Go</button></a></div></div></div><a class="left carousel-control" href="#myCarousel" data-slide="prev">‹</a><a class="right carousel-control" href="#myCarousel" data-slide="next">›</a></div></div>'.format(*info)
+        return slider
+
+    def action_placeorder(self,slots,msg,u_name):
+        foods=[]
+        for e in msg['entities']:
+            if(e['entity']=='food'):
+                foods.append(e['value'])
+        self.orders[u_name]=foods
+        return self.utter("utter_placeorder")
+
+    def action_repeatorder(self,slots,msg,u_name):
+        if(u_name in self.orders):
+            last_order=self.orders[u_name]
+            resp="<ol>"
+            for i in last_order:
+                resp+="<li>"+i+"</li>"
+            resp+='</ol><br>'
+            return resp+self.utter("utter_repeatorder")
+        else:
+            return "Looks like you don't have any past orders . X( "
+
+    def action_addtoorder(self,slots,msg,u_name):
+        if(u_name in self.orders):
+            new_foods=[]
+            for e in msg['entities']:
+                if(e['entity']=='food'):
+                    new_foods.append(e['value'])
+            last_order=self.orders[u_name]
+            order=last_order+new_foods
+            self.orders[u_name]=order
+            resp="<ol>"
+            for i in order:
+                resp+="<li>"+i+"</li>"
+            resp+='</ol><br>'
+            return resp+self.utter("utter_addtoorder")
+        else:
+            return "Looks like you don't have any past orders . X( "
